63190218ca58cfa7d5e03d4cfa766e9a5e7b3f22
Matt Light <matt.light@illuminateed.com>
2015-05-28 16:03:34 -0700

    Reduce coupling in UncryptoTest
    UncryptoTest was dependent on other objects unnecessarily

--- tests/src/Lstr/Upcrypto/UpcryptoTest.php
+++ tests/src/Lstr/Upcrypto/UpcryptoTest.php
@@ -2,120 +2,270 @@
 
 namespace Lstr\Upcrypto;
 
-use Lstr\Upcrypto\CryptoVersionLoader\ArrayCryptoVersionLoader;
 use PHPUnit_Framework_TestCase;
 
 class UpcryptoTest extends PHPUnit_Framework_TestCase
 {
-    public function testEncryptingAndDecryptingAStringReturnsOriginalString()
+    public function testEncryptingCallsEncryptOnTheCryptoAdapter()
     {
-        $test_text = 'something to be encrypted';
-        $versions = [
-            [
-                'CryptoAdapter' => '\Lstr\Upcrypto\CryptoAdapter\DefusePhpEncryption',
-                'CryptoKey' => '\x1...',
-            ],
-        ];
+        $crypto_adapter = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoAdapter\CryptoAdapterInterface'
+            )
+            ->getMock();
+        $crypto_adapter
+            ->expects($this->once())
+            ->method('encrypt')
+            ->with($this->equalTo('unencrypted_string'));
 
-        $version_loader = new ArrayCryptoVersionLoader([
-            $versions[0],
-        ]);
+        $version_loader = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoVersionLoader\AbstractCryptoVersionLoader'
+            )
+            ->setMethods(['getLatestCryptoVersionNumber', 'getCryptoForVersion'])
+            ->getMock();
+        $version_loader
+            ->expects($this->any())
+            ->method('getCryptoForVersion')
+            ->will($this->returnValue($crypto_adapter));
+
+        $upcrypto = new Upcrypto($version_loader);
+
+        $upcrypto->encrypt('unencrypted_string');
+    }
+
+    public function testDecryptingCallsDecryptOnTheCryptoAdapter()
+    {
+        $crypto_adapter = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoAdapter\CryptoAdapterInterface'
+            )
+            ->getMock();
+        $crypto_adapter
+            ->expects($this->once())
+            ->method('decrypt')
+            ->will($this->returnValue('unencrypted_string'));
+
+        $version_loader = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoVersionLoader\AbstractCryptoVersionLoader'
+            )
+            ->setMethods(['getLatestCryptoVersionNumber', 'getCryptoForVersion'])
+            ->getMock();
+        $version_loader
+            ->expects($this->any())
+            ->method('getCryptoForVersion')
+            ->will($this->returnValue($crypto_adapter));
+
         $upcrypto = new Upcrypto($version_loader);
 
         $this->assertEquals(
-            $test_text,
-            $upcrypto->decrypt($upcrypto->encrypt($test_text))
+            'unencrypted_string',
+            $upcrypto->decrypt($upcrypto->encrypt('unencrypted_string'))
+        );
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testDecryptingInvalidEncryptionObjectThrowsAnException()
+    {
+        $version_loader = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoVersionLoader\AbstractCryptoVersionLoader'
+            )
+            ->getMock();
+
+        $upcrypto = new Upcrypto($version_loader);
+
+        $upcrypto->decrypt('something_invalid');
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testDecryptingEncryptionObjectWithoutACipherVersionThrowsAnException()
+    {
+        $version_loader = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoVersionLoader\AbstractCryptoVersionLoader'
+            )
+            ->getMock();
+
+        $upcrypto = new Upcrypto($version_loader);
+
+        $upcrypto->decrypt(json_encode(['cipher' => 'anything']));
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testDecryptingInvalidEncryptionObjectsThrowsAnException()
+    {
+        $version_loader = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoVersionLoader\AbstractCryptoVersionLoader'
+            )
+            ->getMock();
+
+        $upcrypto = new Upcrypto($version_loader);
+
+        $upcrypto->decrypt(json_encode(['crypto_version' => 5]));
+    }
+
+    public function testEncryptingAndDecryptingAStringReturnsOriginalString()
+    {
+        $crypto_adapter = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoAdapter\CryptoAdapterInterface'
+            )
+            ->getMock();
+        $crypto_adapter
+            ->expects($this->once())
+            ->method('encrypt')
+            ->with($this->equalTo('unencrypted_string'))
+            ->will($this->returnValue('encrypted_string'));
+        $crypto_adapter
+            ->expects($this->once())
+            ->method('decrypt')
+            ->with($this->equalTo('encrypted_string'))
+            ->will($this->returnValue('unencrypted_string'));
+
+        $version_loader = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoVersionLoader\AbstractCryptoVersionLoader'
+            )
+            ->setMethods(['getLatestCryptoVersionNumber', 'getCryptoForVersion'])
+            ->getMock();
+        $version_loader
+            ->expects($this->any())
+            ->method('getCryptoForVersion')
+            ->will($this->returnValue($crypto_adapter));
+
+        $upcrypto = new Upcrypto($version_loader);
+
+        $this->assertEquals(
+            'unencrypted_string',
+            $upcrypto->decrypt($upcrypto->encrypt('unencrypted_string'))
         );
     }
 
     public function testUpToDateEncryptionCanBeDetected()
     {
-        $test_text = 'something recently encrypted';
-        $versions = [
-            [
-                'CryptoAdapter' => '\Lstr\Upcrypto\CryptoAdapter\DefusePhpEncryption',
-                'CryptoKey' => '\x1...',
-            ],
-            [
-                'CryptoAdapter' => '\Lstr\Upcrypto\CryptoAdapter\DefusePhpEncryption',
-                'CryptoKey' => '\x2...',
-            ],
-        ];
+        $crypto_adapter = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoAdapter\CryptoAdapterInterface'
+            )
+            ->getMock();
 
-        $version_loader = new ArrayCryptoVersionLoader([
-            $versions[0],
-            $versions[1],
-        ]);
+        $version_loader = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoVersionLoader\AbstractCryptoVersionLoader'
+            )
+            ->setMethods(['getLatestCryptoVersionNumber', 'getCryptoForVersion'])
+            ->getMock();
+        $version_loader
+            ->expects($this->any())
+            ->method('getLatestCryptoVersionNumber')
+            ->will($this->returnValue(2));
+        $version_loader
+            ->expects($this->once())
+            ->method('getCryptoForVersion')
+            ->will($this->returnValue($crypto_adapter));
+
         $upcrypto = new Upcrypto($version_loader);
-        $encrypted = $upcrypto->encrypt(
-            $test_text
-        );
 
         $this->assertTrue(
-            $upcrypto->isUpToDate($encrypted)
+            $upcrypto->isUpToDate($upcrypto->encrypt('recently encrypted'))
         );
     }
 
     public function testOutOfDateEncryptionCanBeDetected()
     {
-        $test_text = 'something historically encrypted';
-        $versions = [
-            [
-                'CryptoAdapter' => '\Lstr\Upcrypto\CryptoAdapter\DefusePhpEncryption',
-                'CryptoKey' => '\x1...',
-            ],
-            [
-                'CryptoAdapter' => '\Lstr\Upcrypto\CryptoAdapter\DefusePhpEncryption',
-                'CryptoKey' => '\x2...',
-            ],
-        ];
+        $crypto_adapter = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoAdapter\CryptoAdapterInterface'
+            )
+            ->getMock();
 
-        $historical_version_loader = new ArrayCryptoVersionLoader([
-            $versions[0],
-        ]);
+        $historical_version_loader = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoVersionLoader\AbstractCryptoVersionLoader'
+            )
+            ->setMethods(['getLatestCryptoVersionNumber', 'getCryptoForVersion'])
+            ->getMock();
+        $historical_version_loader
+            ->expects($this->any())
+            ->method('getLatestCryptoVersionNumber')
+            ->will($this->returnValue(1));
+        $historical_version_loader
+            ->expects($this->once())
+            ->method('getCryptoForVersion')
+            ->will($this->returnValue($crypto_adapter));
+
+        $new_version_loader = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoVersionLoader\AbstractCryptoVersionLoader'
+            )
+            ->setMethods(['getLatestCryptoVersionNumber', 'getCryptoForVersion'])
+            ->getMock();
+        $new_version_loader
+            ->expects($this->any())
+            ->method('getLatestCryptoVersionNumber')
+            ->will($this->returnValue(2));
+
         $historical_upcrypto = new Upcrypto($historical_version_loader);
-        $historically_encrypted = $historical_upcrypto->encrypt(
-            $test_text
-        );
-
-        $new_version_loader = new ArrayCryptoVersionLoader([
-            $versions[0],
-            $versions[1],
-        ]);
         $new_upcrypto = new Upcrypto($new_version_loader);
 
         $this->assertFalse(
-            $new_upcrypto->isUpToDate($historically_encrypted)
+            $new_upcrypto->isUpToDate(
+                $historical_upcrypto->encrypt('historically encrypted')
+            )
         );
     }
 
     public function testEncryptionCanBeUpgraded()
     {
-        $test_text = 'something historically encrypted';
-        $versions = [
-            [
-                'CryptoAdapter' => '\Lstr\Upcrypto\CryptoAdapter\DefusePhpEncryption',
-                'CryptoKey' => '\x1...',
-            ],
-            [
-                'CryptoAdapter' => '\Lstr\Upcrypto\CryptoAdapter\DefusePhpEncryption',
-                'CryptoKey' => '\x2...',
-            ],
-        ];
+        $crypto_adapter = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoAdapter\CryptoAdapterInterface'
+            )
+            ->getMock();
 
-        $historical_version_loader = new ArrayCryptoVersionLoader([
-            $versions[0],
-        ]);
+        $historical_version_loader = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoVersionLoader\AbstractCryptoVersionLoader'
+            )
+            ->setMethods(['getLatestCryptoVersionNumber', 'getCryptoForVersion'])
+            ->getMock();
+        $historical_version_loader
+            ->expects($this->any())
+            ->method('getLatestCryptoVersionNumber')
+            ->will($this->returnValue(1));
+        $historical_version_loader
+            ->expects($this->any())
+            ->method('getCryptoForVersion')
+            ->will($this->returnValue($crypto_adapter));
+
+        $new_version_loader = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoVersionLoader\AbstractCryptoVersionLoader'
+            )
+            ->setMethods(['getLatestCryptoVersionNumber', 'getCryptoForVersion'])
+            ->getMock();
+        $new_version_loader
+            ->expects($this->any())
+            ->method('getLatestCryptoVersionNumber')
+            ->will($this->returnValue(2));
+        $new_version_loader
+            ->expects($this->any())
+            ->method('getCryptoForVersion')
+            ->will($this->returnValue($crypto_adapter));
+
         $historical_upcrypto = new Upcrypto($historical_version_loader);
-        $historically_encrypted = $historical_upcrypto->encrypt(
-            $test_text
-        );
-
-        $new_version_loader = new ArrayCryptoVersionLoader([
-            $versions[0],
-            $versions[1],
-        ]);
         $new_upcrypto = new Upcrypto($new_version_loader);
+
+        $historically_encrypted = $historical_upcrypto->encrypt('historically encrypted');
 
         $this->assertFalse(
             $new_upcrypto->isUpToDate($historically_encrypted)
@@ -126,9 +276,47 @@ class UpcryptoTest extends PHPUnit_Framework_TestCase
         $this->assertTrue(
             $new_upcrypto->isUpToDate($encrypted_with_upgrade)
         );
+    }
+
+    public function testUpToDateEncryptionLeavesEncryptionObjectUnchanged()
+    {
+        $crypto_adapter = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoAdapter\CryptoAdapterInterface'
+            )
+            ->getMock();
+
+        $version_loader = $this
+            ->getMockBuilder(
+                '\Lstr\Upcrypto\CryptoVersionLoader\AbstractCryptoVersionLoader'
+            )
+            ->setMethods(['getLatestCryptoVersionNumber', 'getCryptoForVersion'])
+            ->getMock();
+        $version_loader
+            ->expects($this->any())
+            ->method('getLatestCryptoVersionNumber')
+            ->will($this->returnValue(2));
+        $version_loader
+            ->expects($this->any())
+            ->method('getCryptoForVersion')
+            ->will($this->returnValue($crypto_adapter));
+
+        $upcrypto = new Upcrypto($version_loader);
+
+        $historically_encrypted = $upcrypto->encrypt('historically encrypted');
+
+        $this->assertTrue(
+            $upcrypto->isUpToDate($historically_encrypted)
+        );
+        $encrypted_with_upgrade = $upcrypto->upgradeEncryption(
+            $historically_encrypted
+        );
+        $this->assertTrue(
+            $upcrypto->isUpToDate($encrypted_with_upgrade)
+        );
         $this->assertEquals(
-            $test_text,
-            $new_upcrypto->decrypt($encrypted_with_upgrade)
+            $historically_encrypted,
+            $encrypted_with_upgrade
         );
     }
 }
