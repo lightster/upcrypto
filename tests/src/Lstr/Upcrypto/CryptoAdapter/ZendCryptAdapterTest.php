bc62a499ad17065fb98c67eb39289774266353a4
Matt Light <matt.light@lightdatasys.com>
2017-06-11 00:24:50 -0700

    Remove duplicate adapter test methods
    The parent class defines abstract versions of the test cases, so the
    child class no longer needs to implement those test methods directly

--- tests/src/Lstr/Upcrypto/CryptoAdapter/ZendCryptAdapterTest.php
+++ tests/src/Lstr/Upcrypto/CryptoAdapter/ZendCryptAdapterTest.php
@@ -21,48 +21,6 @@ class ZendCryptAdapterTest extends AbstractAdapterTest
 
     /**
      * @covers ::__construct
-     * @covers ::encrypt
-     * @covers ::decrypt
-     * @covers ::<private>
-     */
-    public function testEncryptingAndDecryptingReturnsTheOriginalString()
-    {
-        $adapter = new ZendCryptAdapter([
-            'crypto_key' => 'the correct password!',
-        ]);
-
-        $known_string = 'this string is known';
-        $this->assertEquals(
-            $known_string,
-            $adapter->decrypt($adapter->encrypt($known_string))
-        );
-    }
-
-    /**
-     * @covers ::__construct
-     * @covers ::encrypt
-     * @covers ::decrypt
-     * @covers ::<private>
-     */
-    public function testUsingTheWrongDecryptionKeyThrowsAnException()
-    {
-        $encryption_adapter = new ZendCryptAdapter([
-            'crypto_key' => 'the correct password!',
-        ]);
-        $decryption_adapter = new ZendCryptAdapter([
-            'crypto_key' => 'the incorrect password!',
-        ]);
-
-        $known_string = 'this string is known';
-        $this->assertFalse(
-            $decryption_adapter->decrypt(
-                $encryption_adapter->encrypt($known_string)
-            )
-        );
-    }
-
-    /**
-     * @covers ::__construct
      * @expectedException \Lstr\Upcrypto\Exception
      */
     public function testFailingToProvideAKeyThrowsAnException()
